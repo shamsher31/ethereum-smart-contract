c8140dc8e19c1272a95825304502953dd965cf9a
shamsher31 <ansarisam47@gmail.com>
2018-10-04 20:26:59 +0530

    Add test to read and update message

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -14,11 +16,22 @@ beforeEach(async () => {
   // use one of the account to deploy contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi Inbox']})
-    .send({ from: accounts[0], gas: '1000000'})
+    .send({ from: accounts[0], gas: '1000000'});
+
+  inbox.setProvider(provider)
 })
 
 describe('Inbox', () => {
-  it('List of accounts', () => {
-    console.log(inbox)
+  it('deployes a contract', () => {
+    assert.ok(inbox.options.address)
+  })
+  it('has a default message', async() => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Hi Inbox')
+  })
+  it('can change the message', async() => {
+    await inbox.methods.setMessage('Bye').send({ from: accounts[0]});
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Bye')
   })
 })
